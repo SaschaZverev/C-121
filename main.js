a2c4e5bd100a18a3843a333537c6a725a8557486
SaschaZverev <98932516+SaschaZverev@users.noreply.github.com>
2022-03-21 16:45:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var previous_result = "";
+
+function setup() 
+{
+  canvas = createCanvas(230, 230);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('Mobilenet', modelLoaded);
+}
+
+function modelLoaded()
+{
+  console.log("Model Loaded !");
+}
+
+function draw()
+{
+  image(video, 0, 0, 230, 230);
+  classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+  if (error)
+  {
+    console.error(error);
+  }
+  else
+  {
+    if((results[0].confidence > 0.5) && (previous_result != results[0].label))
+    {
+      console.log(results);
+      previous_result = results[0].label;
+      var synth = window.speechSynthesis;
+      speak_data = "Object detected is - " + results[0].label;
+      var utterThis = new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
+
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+  }
+}
